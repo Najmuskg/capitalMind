e42b42775df02a0033ae1624f2485279ebfcca54
Sakib <najmuskg@gmail.com>
2023-01-10 17:19:10 +0600

    Client-Stories Page Created

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -264,8 +264,21 @@ $(function () {
     slidesPerView:"auto",
     freeMode: true,
     spaceBetween: 10,
-
-
+  });
+  new Swiper(".bannerSlider", {
+    speed:500,
+    slidesPerView:"auto",
+    freeMode: true,
+    spaceBetween: 10,
+    pagination: {
+      el: ".quote-progress",
+      type: "progressbar",
+    },
+    navigation: {
+      nextEl: ".quote-next",
+      prevEl: ".quote-prev",
+      
+    },
   });
 
   new Swiper(".transactionSlider", {
